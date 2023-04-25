1fbbf74cf39fc7536754ac70979e68be3a17ca58
Ramiro Aragon Paz <ramiroaragonpaz@gmail.com>
2023-04-24 21:35:23 -0300

    Initial commit

---
+++ src/components/chatgpt.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+  apiKey: process.env.API_KEY,
+});
+
+    const question= async (prompt)=> {
+
+    const openai = new OpenAIApi(configuration);
+    const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: prompt,
+        max_tokens: 2000,
+        temperature: 0,
+    })
+        .then((responseAnswer)=> {
+        console.log(responseAnswer.data.choices[0].text);
+        let chatgpt = (responseAnswer.data.choices[0].text)
+        console.log("ChatGPT answer: ", chatgpt);
+        return responseAnswer.data.choices[0].text;
+        })
+    return response
+}
+
+export default question;
